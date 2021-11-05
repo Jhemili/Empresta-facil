6662b817136a048ea3f45e61ff5ccf080dfe11d5
Jhemili <51094480+Jhemili@users.noreply.github.com>
2021-11-05 14:28:27 -0300

    Estrutura do sistema em html, css, php e js

---
+++ EMPRESTA-FACIL/inventario.php
@@ -0,0 +1,17 @@
+<?php include "head.php" ?>
+<body>
+    <?php include "side-menu.php" ?>
+    <main>
+        <div class="card">
+            <div class="container-titulos">
+                <h1>Inventário</h1>
+                <h2>Gerencie seus itens.</h2>
+            </div>       
+            <div class="container container-itens">
+                
+            <button type="submit">cadastrar novo</button>  
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ EMPRESTA-FACIL/side-menu.php
@@ -0,0 +1,14 @@
+<div class="side-menu">
+    <div id="menu" onclick="onClickMenu()">
+        <span class="line" id="line1"></span>
+        <span class="line" id="line2"></span>
+        <span class="line" id="line3"></span>
+    </div>    
+    <ul class="nav" id="nav">
+        <li><a href="inventario.php">Inventário</a></li>
+        <li><a href="itens-emprestados.php">Itens Emprestados</a></li>
+        <li><a href="perfil.php">Perfil</a></li>
+        <li><a href="index.php">Sair</a></li>
+    </ul>    
+</div>
+<script src="script.js"></script>
