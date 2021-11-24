267d9b6e7349cf6785fce75fa4c8de764f68bea6
Jhemili <jhemili.santos@gmail.com>
2021-11-24 18:31:49 -0300

    excluir destinatarios

--- receber_item.php
+++ receber_item.php
@@ -13,9 +13,18 @@ if(isset($_POST['receber'])){
     $idItem = filter_input(INPUT_POST,'idItem');
     $now = new DateTime();
     $now = $now->format('Ymd');    
-
+    
     $updateItem = "UPDATE itens SET estatus = 0, data_retorno = '$now' WHERE id_item = '$idItem'"; 
-    $updateItem = $conn->query($updateItem);
+    $updateItem = $conn->query($updateItem)or die($conn->error);
+ 
+    $sqlDest = "SELECT destinatario FROM itens WHERE id_item = '$idItem'"; 
+    $sqlDest = $conn->query($sqlDest) or die($conn->error);  
+    while($dados = $sqlDest->fetch_assoc()){
+        $idDest = $dados['destinatario'];
+    }
+    $updateDest = "DELETE FROM destinatario WHERE destinatario.id_destinatario = '$idDest'";
+    $updateDest = $conn->query($updateDest)or die($conn->error);
+
 
 }
 
